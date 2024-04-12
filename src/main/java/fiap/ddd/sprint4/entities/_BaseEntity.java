b4060d82e6a364759c7e40ba1f65383b4df875ff
VICTOR <victorzarbesu@gmail.com>
2024-04-12 16:41:05 -0300

    Criando _BaseEntity para gerar IDs para as entidades

---
+++ src/main/java/fiap/ddd/sprint4/entities/_BaseEntity.java
@@ -0,0 +1,29 @@
+package fiap.ddd.sprint4.entities;
+
+
+public abstract class _BaseEntity {
+    private int id;
+
+    public _BaseEntity(){
+
+    }
+
+    public _BaseEntity(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "_BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
